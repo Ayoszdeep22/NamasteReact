0237dd59d2226d4393a778f8fe41d7220ba47ec4
Ayoszdeep22 <ayoszdeepmishra2004@gmail.com>
2025-08-24 14:05:54 +0530

    create-react-app

--- app.js
+++ app.js
@@ -12,36 +12,27 @@
  * 
  * 
  */
-const heading1=React.createElement("div",{id:"parent"},
-  [React.createElement("div",{id:"child"},
-    [React.createElement("h1",{id:"heading"},"this is the heading tag"),
-      React.createElement("h2",{id:"heading"},"this is the heading2 tag")]),
-    React.createElement("div",{id:"child2"},
-    [React.createElement("h1",{id:"heading"},"this is the heading tag"),
-      React.createElement("h2",{id:"heading"},"this is the heading2 tag")])]);
+// nested div in react including 
 
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const heading = React.createElement(
-  "h1",
-  { id: "helloId" },  // Note the quotes around "helloId"
-  "Hello from the Namaste React"
+const heading1 = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" },
+    React.createElement("h1", { id: "heading1" }, "This is the H1 heading"),
+    React.createElement("h2", { id: "heading2" }, "This is the H2 heading")
+  ),
+  React.createElement("div", { id: "child2" },  
+    React.createElement("h1", { id: "heading3" }, "This is another H1 heading"),
+    React.createElement("h2", { id: "heading4" }, "This is another H2 heading")
+  )
 );
 
-// it is basically creating a object not an element it have three things props attributes and elements
+const heading = React.createElement("h1", { id: "helloId" }, "Hello from Namaste React");
+
+
+// it is basically creating a object not an element it have three things props attributes and elements above one
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading1);///object->element
